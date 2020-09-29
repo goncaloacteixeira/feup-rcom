d65f2ef2c1f74e2c83dab4e04c756a49971342d0
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2020-09-29 15:15:41 +0100

    updated [fixed]

--- tp01/writenoncanonical.c
+++ tp01/writenoncanonical.c
@@ -12,33 +12,32 @@
 #define FALSE 0
 #define TRUE 1
 
-volatile int STOP=FALSE;
+volatile int STOP = FALSE;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     int fd,c, res;
     struct termios oldtio,newtio;
     char buf[255];
     int i, sum = 0, speed = 0;
-    
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+
+    if ( (argc < 2) ||
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
   	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
     }
 
-
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
-
+    /*
+      Open serial port device for reading and writing and not as controlling tty
+      because we don't want to get killed if linenoise sends CTRL-C.
+    */
 
     fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+    if (fd <0) {
+      perror(argv[1]); exit(-1);
+    }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    if (tcgetattr(fd, &oldtio) == -1) { /* save current port settings */
       perror("tcgetattr");
       exit(-1);
     }
@@ -54,52 +53,41 @@ int main(int argc, char** argv)
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
     newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
 
-
-
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
-  */
-
-
+    /*
+      VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+      leitura do(s) prï¿½ximo(s) caracter(es)
+    */
 
     tcflush(fd, TCIOFLUSH);
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
 
     printf("New termios structure set\n");
 
-
-
+    /*
     for (i = 0; i < 255; i++) {
       buf[i] = 'a';
     }
-    
-    /*testing*/
-    buf[25] = '\n';
-    
-    res = write(fd,buf,255);   
+    */
+
+    printf("Input: ");
+    int chars = gets(buf);
+
+    res = write(fd, buf, 255);
     printf("%d bytes written\n", res);
- 
 
-  /* 
-    O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar 
-    o indicado no guião 
-  */
+    /*
+      O ciclo FOR e as instruï¿½ï¿½es seguintes devem ser alterados de modo a respeitar
+      o indicado no guiï¿½o
+    */
 
-
-
-   
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
-
-
-
 
     close(fd);
     return 0;
