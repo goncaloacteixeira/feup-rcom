6efaca19cdf64e08732475d67aedde7b55e2e361
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2020-10-13 19:28:55 +0100

    added source for application header

---
+++ tp03/application.c
@@ -0,0 +1,25 @@
+#include "application.h"
+
+int llopen(int port, int type) {
+  char file[48];
+  sprintf(file, "/dev/ttyS%d", port);
+
+  if (type == TRANSMITTER)
+    return open_writer(file);
+  else if (type == RECEIVER)
+    return 0; /* todo - substituir por open_reader quando o ´noncanonical` estiver pronto */
+
+  return -1;
+}
+
+int llclose(int fd) {
+  return 0; /* TODO yet */
+}
+
+int llwrite(int fd, char* buffer, int length) {
+  return 0; /* TODO yet */
+}
+
+int llread(int fd, char* buffer) {
+  return 0; /* TODO yet */
+}
--- tp03/application.h
+++ tp03/application.h
@@ -3,26 +3,10 @@
 #define TRANSMITTER 0
 #define RECEIVER    1
 
-int llopen(int port, int type) {
-  char file[48];
-  sprintf(file, "/dev/ttyS%d", port);
+int llopen(int port, int type);
 
-  if (type == TRANSMITTER)
-    return open_writer(file);
-  else if (type == RECEIVER)
-    return 0; /* todo - substituir por open_reader quando o ´noncanonical` estiver pronto */
+int llclose(int fd);
 
-  return -1;
-}
+int llwrite(int fd, char* buffer, int length);
 
-int llclose(int fd) {
-  return 0; /* TODO yet */
-}
-
-int llwrite(int fd, char* buffer, int length) {
-  return 0; /* TODO yet */
-}
-
-int llread(int fd, char* buffer) {
-  return 0; /* TODO yet */
-}
+int llread(int fd, char* buffer);
